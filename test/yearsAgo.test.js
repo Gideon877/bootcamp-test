f84ae809a3f00b72ce0a33b2a6fb6765d892549b
Thabang Gideon Magaola <thabang@getwala.com>
2018-03-08 11:39:55 +0200

    Update yearsAgo.test.js

--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,6 +1,6 @@
 describe('yearsAgo function', function(){
 
     it('takes in a year and return how many years ago that was', function(){
-        assert.equal(yearsAgo(1990), 27);
+        assert.equal(yearsAgo(1991), 27);
       });
 });
